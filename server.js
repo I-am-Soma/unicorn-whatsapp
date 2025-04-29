861ca0a9c22e2f12f6cb7e5b5c1e8639c73baae2
I-am-Soma <javirambar@gmail.com>
2025-04-28 23:35:04 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Variables de entorno
 const supabaseUrl = process.env.SUPABASE_URL;
